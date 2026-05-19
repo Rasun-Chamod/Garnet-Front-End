e61e6ad670f790cb63eeeb91c1f312bf11dd6176
Rasun-Chamod <w.a.r.chamod@gmail.com>
2026-05-20 04:07:31 +0530

    Editing PageTransition.tsx

--- Garnet-Front-End/src/components/PageTransition/PageTransition.tsx
+++ Garnet-Front-End/src/components/PageTransition/PageTransition.tsx
@@ -13,7 +13,11 @@ const transition = {
 const variants = {
   initial: { opacity: 0, y: 24 },
   animate: { opacity: 1, y: 0, transition },
-  exit: { opacity: 0, y: -24, transition: { duration: 0.5, ease: [0.4, 0, 1, 1] } },
+  exit: {
+    opacity: 0,
+    y: -24,
+    transition: { duration: 0.5, ease: [0.4, 0, 1, 1] },
+  },
 };
 
 function PageTransition({ children }: PageTransitionProps) {
